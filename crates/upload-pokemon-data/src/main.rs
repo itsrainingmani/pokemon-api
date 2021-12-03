26c40488de7df7327533e20863a824eae7a7e2f6
Manikandan Sundararajan <mani@hey.com>
2021-12-03 18:30:51 -0500

    import pokemon csv mod in main

--- crates/upload-pokemon-data/src/main.rs
+++ crates/upload-pokemon-data/src/main.rs
@@ -1,8 +1,13 @@
+mod pokemon_csv;
+use pokemon_csv::*;
+
 fn main() -> Result<(), csv::Error> {
+    // Since the main function's return type is the same as the error type,
+    // we can use ? on the Result<Reader<File>, csv::Error> to turn into a Reader<File>
     let mut rdr = csv::Reader::from_path("./crates/upload-pokemon-data/pokemon.csv")?;
 
-    for result in rdr.records() {
-        let record = result?;
+    for result in rdr.deserialize() {
+        let record: PokemonCsv = result?;
         println!("{:?}", record);
     }
 
